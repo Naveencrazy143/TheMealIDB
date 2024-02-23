d69b3e6ac135659fc7bd5fb0e2d92f91d599510b
INBOOK_X1_SLIM\navee <naveen.intern@leorainfotech.in>
2024-02-24 01:59:56 +0530

    done

--- src/Module/Screen/Footer/index.js
+++ src/Module/Screen/Footer/index.js
@@ -7,19 +7,19 @@ function Footer() {
         < div className="row">
 
       
-          <div className="col-lg-4">
+          <div className="col-lg-4 col-md-4">
             <p>&copy; 2024 TheMealDB Proudly build in India</p>
           </div>
         
 
-          <div className="col-lg-4 ">
+          <div className="col-lg-4 col-md-4">
             <h5>CONTACT US</h5>
             <p><i className="fas fa-home"></i> Address: 123 Street, City, Country</p>
 <p><i className="fas fa-phone"></i> Phone: 6379955228</p>
 <p><i className="fas fa-envelope"></i> Email: naveen.jdit@gmail.com</p>
 
           </div>
-          <div className="col-lg-4 ">
+          <div className="col-lg-4 col-md-4">
             <h5>Quick Links</h5>
             <ul className="list-unstyled">
               <li><a href="#">Home</a></li>
