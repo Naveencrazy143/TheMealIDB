ee608855213b2110b3e7712e4e551f509182645b
INBOOK_X1_SLIM\navee <naveen.intern@leorainfotech.in>
2024-02-24 01:09:35 +0530

    all new

--- src/Module/Container/MenuCard/index.js
+++ src/Module/Container/MenuCard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MenuCard({ src, text ,onClick}) {
   return (
-      <div className="col-lg-3 my-lg-3 col-md-4" onClick={onClick}>
+      <div className="col-lg-3 my-lg-2 col-md-4" onClick={onClick}>
         <div className="card" style={{backgroundColor:"#2d2013"}}>
           <img className="card-img-top" src={src} alt="Card image cap" />
           <div className="card-body ">
